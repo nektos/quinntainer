c350cd9b755ffd5014f3ebc5904fe614be6e8932
Casey Lee <cplee@nektos.com>
2022-03-19 23:19:32 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# quinntainer
+# Overview
+
+[![](https://img.shields.io/github/v/release/nektos/quinntainer?style=flat-square)](https://github.com/nektos/quinntainer/releases/latest) [![](https://img.shields.io/npm/v/quinntainer?style=flat-square)](https://www.npmjs.com/package/quinntainer)
+
+This repo contains an action for deploying containers to AWS. It will build a docker image from a `Dockerfile` in the repo, push the image to [Amazon ECR](https://aws.amazon.com/ecr/) and the run the container via [AWS AppRunner](https://aws.amazon.com/apprunner/).
+
+The repo also contains a CLI tool to setup the GitHub workflow in your repo and also setup the AWS IAM role necessary for GitHub Actions to assume via [OpenID Connect](https://docs.github.com/en/actions/deployment/security-hardening-your-deployments/configuring-openid-connect-in-amazon-web-services).
+
+![](overview.png)
+
+# Quickstart
+
+To setup your application for _quinntainers_, run the following from within your application directory.
+
+```
+npx quinntainer init
+```
+
+# Why?
+
+This tool was inspired by a tweet from [Corey Quinn](https://twitter.com/QuinnyPig) following his experiences with Google Cloud Run. He's right - for his use case, Google Cloud Run is much easier. Therefore, the only appropriate solution is to create the `18th` way to run containers on AWS 🚀
+
+![](why.png)
+
+# Example Workflow
+
+```
+name: Deploy
+on:
+  push:
+    branches: 
+    - main
+
+permissions:
+  id-token: write
+  contents: read
+
+jobs:
+  deploy:
+    concurrency: deploy
+    runs-on: ubuntu-latest
+    steps:
+    - uses: nektos/quinntainer@v1
+      with: 
+        aws-region: us-west-2
+        role-to-assume: arn:aws:iam::0123456789:role/quinntainer-gha-myorg-myrepo
+        port: 80
+```
+
+# Action Inputs
+
+The actions supports the following inputs:
+
+| Input | Description |
+| ----- | ----------- | 
+| `aws-region` | AWS Region, e.g. us-east-2 |
+| `role-to-assume` | ARN of IAM Role to assume via GHA OIDC |
+| `port` | Port listening in container |
+| `cpu` | CPU to allocate to container (Default: `1024`) |
+| `memory` | Memory to allocate to container (Default: `2048`) |
+| `docker-build-context` | Path to docker build context (Default: `.`) |
+| `dockerfile` | Path to Dockerfile (Default: `Dockerfile`) |
---
+++ bin/index.js
@@ -0,0 +1,212 @@
+#! /usr/bin/env node
+
+require('pkginfo')(module, 'version');
+const yargs = require("yargs/yargs");
+const { hideBin } = require('yargs/helpers');
+const ora = require('ora');
+const git = require('isomorphic-git');
+const Mustache = require('mustache');
+const fs = require('fs');
+const http = require('isomorphic-git/http/node');
+const path = require('path');
+const GitUrlParse = require("git-url-parse");
+const Diff = require('diff');
+require('colors');
+const inquirer = require('inquirer')
+const { 
+  CloudFormationClient, 
+  CreateStackCommand,
+  UpdateStackCommand,
+  DescribeStacksCommand,
+  DeleteStackCommand,
+  waitUntilStackCreateComplete,
+  waitUntilStackUpdateComplete,
+  waitUntilStackDeleteComplete,
+ } = require("@aws-sdk/client-cloudformation");
+
+
+const doInit = async (argv) => {
+  const repoInfo = await getRepoInfo();
+  const providerStack = await upsertProviderStack();
+  const oidcProvider = providerStack.Outputs
+    .filter(o => o.OutputKey === 'OidcProvider')
+    .map(o => o.OutputValue)[0];
+
+  const roleStack = await upsertRoleStack(repoInfo.org, repoInfo.repo, oidcProvider);
+  const role = roleStack.Outputs
+    .filter(o => o.OutputKey === 'Role')
+    .map(o => o.OutputValue)[0];
+
+  const region = roleStack.StackId.split(':')[3];
+
+  const version = `v${module.exports.version}`;
+
+  return setupWorkflow({
+    region, 
+    role, 
+    branch: repoInfo.branch,
+    root: repoInfo.root,
+    version,
+  });
+};
+
+const getRepoInfo = async () => {
+  const gitroot = await git.findRoot({fs, filepath: process.cwd()});
+  const remotes = await git.listRemotes({fs, dir: gitroot});
+  const remoteUrl = GitUrlParse(remotes.filter(r => r.remote == 'origin').map(r => r.url)[0]);
+  return {
+    org: remoteUrl.organization,
+    repo: remoteUrl.name,
+    branch: await git.currentBranch({fs, dir: gitroot, fullname: false}),
+    root: gitroot,
+  };
+};
+
+const upsertProviderStack = async () => {
+  return upsertStack({
+    stackName: 'quinntainer-common',
+    templateName: 'cfn-common.yaml',
+    creationConfirmation: 'OK to create a new IAM OIDC Provider for GitHub Actions?',
+  });
+};
+const upsertRoleStack = async (org, repo, oidcProvider) => {
+  const stackParameters = [
+    {ParameterKey: 'GitHubOrg', ParameterValue: org},
+    {ParameterKey: 'RepositoryName', ParameterValue: repo},
+    {ParameterKey: 'OidcProvider', ParameterValue: oidcProvider},
+  ];
+  return upsertStack({
+    stackName: `quinntainer-role-${org}-${repo}`,
+    templateName: 'cfn-role.yaml',
+    capabilities: ["CAPABILITY_NAMED_IAM"],
+    stackParameters,
+    creationConfirmation: 'OK to create a new IAM role for GitHub Actions to assume?',
+  });
+};
+
+const upsertStack = async (params) => {
+  const { stackName, templateName, stackParameters, capabilities, creationConfirmation } = params;
+  const client = new CloudFormationClient();
+  let exists = false;
+  try {
+    const resp = await client.send(new DescribeStacksCommand({
+      StackName: stackName,
+    }));
+    exists = true;
+    if(resp.Stacks[0].StackStatus === 'ROLLBACK_COMPLETE') {
+      const spinner = ora(`Cleaning up failed stack: ${stackName}`).start();
+      await client.send(new DeleteStackCommand({ StackName: stackName }));
+      await waitUntilStackDeleteComplete({client}, {
+        StackName: stackName,
+      });
+      spinner.stop();
+      exists = false;
+    }
+  } catch (e) {
+    if(e.message !== `Stack with id ${stackName} does not exist`) {
+      throw e;
+    }
+  }
+  const template = fs.readFileSync(path.resolve(__dirname, `../${templateName}`));
+  if(exists) {
+    const spinner = ora(`Updating stack: ${stackName}`).start();
+    try {
+      const resp = await client.send(new UpdateStackCommand({
+        StackName: stackName,
+        TemplateBody: template,
+        Parameters: stackParameters,
+        Capabilities: capabilities,
+      }));
+      await waitUntilStackUpdateComplete({client}, {
+        StackName: stackName,
+      });
+    } catch (e) {
+      if(e.message !== `No updates are to be performed.`) {
+        throw e;
+      }
+    } finally {
+      spinner.stop();
+    }
+  } else {
+    if(creationConfirmation) {
+      const answers = await inquirer.prompt([{
+        type: "confirm",
+        name: "approve",
+        message: creationConfirmation,
+        default: true,
+      }]);
+      if(answers.approve !== true) {
+        throw new Error("Exiting");
+      }
+    }
+    const spinner = ora(`Creating stack: ${stackName}`).start();
+    await client.send(new CreateStackCommand({
+      StackName: stackName,
+      TemplateBody: template,
+      Parameters: stackParameters,
+      Parameters: stackParameters,
+      Capabilities: capabilities,
+    }));
+    await waitUntilStackCreateComplete({client}, {
+      StackName: stackName,
+    });
+    spinner.stop();
+  }
+  const resp = await client.send(new DescribeStacksCommand({
+    StackName: stackName,
+  }));
+  return resp.Stacks[0]
+};
+
+const setupWorkflow = async (config) => {
+    const answers = await inquirer.prompt([{
+      type: "number",
+      name: "port",
+      message: "What port does your container expose?",
+      default: 8080,
+    }]);
+    config.port = answers.port;
+    const template = fs.readFileSync(path.resolve(__dirname, '../gha-deploy.yaml')).toString('utf8');
+    const newWorkflow = Mustache.render(template, config);
+
+    const workflowRelPath = '.github/workflows/quinntainer-deploy.yaml';
+    const workflowPath = path.resolve(config.root, workflowRelPath);
+
+    if(fs.existsSync(workflowPath)) {
+      const existingWorkflow = fs.readFileSync(workflowPath).toString('utf8');
+      const diff = Diff.diffLines(existingWorkflow, newWorkflow);
+      if(diff.filter(part => part.added || part.removed).length) {
+        diff.forEach((part) => {
+          // green for additions, red for deletions
+          // grey for common parts
+          const color = part.added ? 'green' :
+            part.removed ? 'red' : 'grey';
+          process.stderr.write(part.value[color]);
+        });
+        const answers = await inquirer.prompt([{
+          type: "confirm",
+          name: "approve",
+          message: `OK to overwrite ${workflowPath} with these changes?`,
+          default: true,
+        }]);
+        if(answers.approve !== true) {
+          throw new Error("Exiting");
+        }
+      } else {
+        // no changes
+        return;
+      }
+    }
+
+    fs.mkdirSync(path.dirname(workflowPath), { recursive: true });
+    fs.writeFileSync(workflowPath, newWorkflow);
+    console.log("Changes to workflow file have been made. You now need to commit and push these to take effect.")
+};
+
+yargs(hideBin(process.argv))
+  .command('init', 'init the repo to run on containers', () => {}, doInit)
+  .demandCommand()
+  .strict()
+  .showHelpOnFail(true)
+  .help(true)
+  .argv
